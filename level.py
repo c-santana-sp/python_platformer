3485cf4702e4306fb8f9df18e027846a1936d632
c-santana-sp <eduardomarquesdesantana@gamil.com>
2022-08-11 23:53:09 -0300

    feat: building level

--- level.py
+++ level.py
@@ -1,5 +1,7 @@
 import pygame
 from settings import *
+from tile import Tile
+from player import Player
 
 class Level:
 
@@ -10,5 +12,18 @@ class Level:
     self.active_sprites = pygame.sprite.Group()
     self.collision_sprites = pygame.sprite.Group()
 
+    self.setup_level()
+
+  def setup_level(self):
+    for row_index, row in enumerate(LEVEL_MAP):
+      # print(f'{row_index}:{row}-> y = {row_index * TILE_SIZE}')
+      for column_index, column in enumerate(row):
+        x = column_index * TILE_SIZE
+        y = row_index * TILE_SIZE
+        if column == 'X':
+          Tile((x, y), [self.visible_sprites])
+        if column == 'P':
+          Player((x, y), [self.visible_sprites])
+
   def run(self):
-    pass
+    self.visible_sprites.draw(self.display_surface)
