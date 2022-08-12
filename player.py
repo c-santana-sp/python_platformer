52ecfa8097e07dda4e071ddfb2d235014a0d45ff
c-santana-sp <eduardomarquesdesantana@gamil.com>
2022-08-11 23:39:40 -0300

    feat: level and player classes

---
+++ player.py
@@ -0,0 +1,11 @@
+import pygame
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+
+  def __init__(self, position, groups):
+    super().__init__(groups)
+
+    self.image = pygame.Surface((TILE_SIZE // 2, TILE_SIZE))
+    self.image.fill(PLAYER_COLOR)    
+    self.rect = self.image.get_rect(topleft = position)
